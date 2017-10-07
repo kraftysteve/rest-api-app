dc03ddb7089aa92b94c1ba19787c3e2fc7f7a4a8
kraftysteve <stevestravelling@gmail.com>
2017-10-07 20:49:39 +1100

    Fixed not importing db

--- app.py
+++ app.py
@@ -31,8 +31,9 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
 
-    @app.before_first_request
-    def create_table():
-        db.create_all()
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_table():
+            db.create_all()
 
     app.run(port=5000, debug=True)
