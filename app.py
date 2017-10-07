e3d0f09683b5eb499dd12bce661e8167b68dface
kraftysteve <stevestravelling@gmail.com>
2017-10-07 19:34:45 +1100

    Add debug

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'random_key'
